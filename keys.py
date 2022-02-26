f720424aa7af8a7e21e60ca7dbf2a2da4171b37f
iHazzu <francisvexu@gmail.com>
2022-02-25 23:35:09 -0300

    v1

--- keys.py
+++ keys.py
@@ -5,7 +5,6 @@ from requests.auth import HTTPBasicAuth
 
 DISCORD_BOT_TOKEN = env["DISCORD_BOT_TOKEN"]
 WP_CREDENCIALS = env["WORDPRESS_USER"] + ':' + env["WORDPRESS_PASSWORD"]
-WP_TOKEN = base64.b64encode(WP_CREDENCIALS.encode())
-WP_AUTH_HEADER = {'Authorization': 'Basic ' + WP_TOKEN.decode('utf-8')}
+WP_AUTH_HEADER = {'Authorization': 'Basic ' + WP_CREDENCIALS}
 WP_AUTH = HTTPBasicAuth(env["WORDPRESS_USER"], env["WORDPRESS_PASSWORD"])
 TWITTER_BEARER_TOKEN = env["TWITTER_BEARER_TOKEN"]
